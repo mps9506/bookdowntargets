8f98a082d608501e704493e87fa32d2621968046
Michael Schramm <michael.schramm@agnet.tamu.edu>
2024-06-13 14:04:28 -0400

    fix package documentation

--- R/package.R
+++ R/package.R
@@ -4,8 +4,9 @@
 #'   Statistics and data science. The `tarchetypes` package provides
 #'   convenient helper functions to create specialized targets, making
 #'   pipelines in targets easier and cleaner to write and understand.
-#' @name tarchetypes-package
+#' @name bookdowntargets-package
 #' @importFrom fs dir_ls is_dir path_ext_remove path_rel
+#' @importFrom targets tar_assert_chr tar_assert_package tar_assert_path tar_deparse_language tar_option_get tar_target_raw tar_tidy_eval
 #' @importFrom withr local_options
 NULL
 
