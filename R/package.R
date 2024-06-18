9178a3e5e29bce8d1320a984fcdb795b06d95f51
Michael Schramm <michael.schramm@agnet.tamu.edu>
2024-06-18 08:41:19 -0400

    documentation improvements

--- R/package.R
+++ R/package.R
@@ -1,9 +1,8 @@
-#' targets: Archetypes for Targets
+#' bookdowntargets: tarchetype pipeline for bookdown reports.
 #' @description A pipeline toolkit for R, the `targets` package brings together
 #'   function-oriented programming and Make-like declarative pipelines for
-#'   Statistics and data science. The `tarchetypes` package provides
-#'   convenient helper functions to create specialized targets, making
-#'   pipelines in targets easier and cleaner to write and understand.
+#'   Statistics and data science. The `bookdowntargets` package provides a
+#'   convenient helper function to create specialized targets.
 #' @name bookdowntargets-package
 #' @importFrom fs dir_ls is_dir path_ext_remove path_rel
 #' @importFrom targets tar_assert_chr tar_assert_package tar_deparse_language tar_option_get tar_target_raw tar_tidy_eval
